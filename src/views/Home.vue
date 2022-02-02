0f583064bbad11ce09ec6f55aab53b452307a0d8
luimar96 <luisa_martensson@hotmail.com>
2022-02-02 15:22:02 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img src="../assets/people.png" alt="people" />
+    <HelloWorld msg="DoIT" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
