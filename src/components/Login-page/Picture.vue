54b83de439e3d5637b9291f85b198ccb3965a431
luimar96 <luisa_martensson@hotmail.com>
2022-02-03 10:51:19 +0100

    login page

---
+++ src/components/Login-page/Picture.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <img src="@/assets/people.png" alt="people" />
+        <h1 >DoIT</h1>           
+        <p>Helps you to better manage your task</p>         
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+*{
+    font-family: 'Roboto', sans-serif;
+}
+img{
+    width: 40em;
+}
+h1{
+   font-size:144px;
+   margin:0;
+   margin-right: 1em;
+}
+p{
+    margin: 0px;
+    font-size: 40px;
+    opacity: 50%;
+    margin-left: 40px;
+    font-weight: bold;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,44 @@
-<template>
-  <div class="row">
-    <div class="column" >
-     <img src="../assets/people.png" alt="people" />
-     <h1 >DoIT</h1>
-     <p>Helps you to better manage your task</p>
-   </div>
+<template >
+<div class="row">
+  <div class="column">
+    <Picture/>
+  </div>
+  <div class="column login-container">
    <Login/>
   </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Login from "../components/Login.vue";
-//import HelloWorld from "@/components/HelloWorld.vue";
-
+import Login from "../components/Login-page/Login.vue";
+import Picture from "../components/Login-page/Picture.vue"
 export default {
   name: "Home",
   components: {
-    Login
+    Login,
+    Picture
   },
 };
 </script>
 <style scoped>
+
 * {
   box-sizing: border-box;
 }
+
+.row {
+  display: flex;
+}
+
+.column {
+  flex: 50%;
+  padding: 10px;
+  height: 300px; 
+}
+
+.login-container{
+  margin-top:25em;
+}
+
 </style>
