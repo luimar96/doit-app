1dbacecb6b433746f609427901eb5b37b5eb4caf
luimar96 <luisa_martensson@hotmail.com>
2022-02-03 19:29:22 +0100

    Close button & rename folder name

---
+++ src/components/Buttons/CloseButton.vue
@@ -0,0 +1,36 @@
+<template>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div>
+    <button class="close-btn">
+        <i class="fa fa-close"></i>
+    </button>
+</div> 
+    
+</template>
+<script>
+export default {
+    components: {
+
+    }
+}
+</script>
+
+<style scoped>
+.close-btn{
+    width: 70px;
+    height: 70px;
+    border-radius: 1em;
+    border: none;
+    background-color: #EEB3B3;
+    box-shadow: 0px 3px 8px 1px #888888;
+    cursor: pointer;
+}
+.fa-close{
+    font-size: 2em;
+     opacity: 50%;
+}
+.close-btn:hover {background-color: #ee9999}
+.close-btn:active {
+  transform: translateY(4px);
+}
+</style>
