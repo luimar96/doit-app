ff4e68ce142bcc760f442956390f0b7a2055f37d
Moosewell <max@melander.biz>
2022-02-06 19:10:11 +0100

    Splitting parts of OverviewPage into additional components for the sake of a simpler system.

---
+++ src/components/ProjectGrid.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="project-grid">
+		<div class="grid-item"/>
+		<div class="grid-item"/>
+		<div class="grid-item"/>
+		<div class="grid-item"/>
+		<div class="grid-item"/>
+		<div class="grid-item"/>
+		<div class="grid-item"/>
+		<div class="grid-item"/>
+    </div>
+</template>
+<script>
+
+export default ({
+
+})
+</script>
+
+<style scoped>
+.project-grid{
+    padding-bottom: 70px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.grid-item{
+    margin: 2vw;
+    width: 28vw;
+    min-width: 380px;
+    height: 200px;
+    background-color: #DEDCFF;
+}
+</style>
--- src/views/OverviewPage.vue
+++ src/views/OverviewPage.vue
@@ -1,29 +1,11 @@
 <template>
   <div class="container">
     <div class="grid-choices">
-        <Searchbar class="searchbar"/>
-        <AddButton class="addbutton"/>
+      <Searchbar class="searchbar"/>
+      <AddButton class="addbutton"/>
     </div>
-    <div class="project-grid">
-      <div class="grid-item">
-      </div>
-      <div class="grid-item">
-      </div>
-      <div class="grid-item">
-      </div>
-      <div class="grid-item">
-      </div>
-      <div class="grid-item">
-      </div>
-      <div class="grid-item">
-      </div>
-      <div class="grid-item">
-      </div>
-      <div class="grid-item">
-      </div>
-    </div>
+    <ProjectGrid/>
     
-
     <!--<button @click="isOpen = true">Show Modal</button>
     <transition name="fade" apper>
     <modal :open="isOpen" @close="isOpen = !isOpen">
@@ -37,12 +19,14 @@
 <script>
 import AddButton from "../components/Buttons/AddButton.vue"
 import Searchbar from "../components/Searchbar.vue"
+import ProjectGrid from "../components/ProjectGrid.vue"
 //import {ref} from "vue"
 
 export default {
   components:{
     AddButton,
-    Searchbar
+    Searchbar,
+    ProjectGrid
   },
   setup () {
       const isOpen = (false) //Ref(false)
@@ -72,22 +56,6 @@ export default {
 .addbutton{
   width: 30vw;
   margin-left: 25px;
-}
-
-.project-grid{
-  padding-bottom: 70px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
-/* Luisa! --> grid-item styling below should be moved into the 'ProjectItem' component*/
-.grid-item{
-  margin: 2vw;
-  width: 28vw;
-  min-width: 380px;
-  height: 200px;
-  background-color: #DEDCFF;
 }
 
 </style>
