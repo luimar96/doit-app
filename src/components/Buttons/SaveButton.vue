de033d3f5550d3e6eac031fe520cf20c9ca6a8e8
Moosewell <max@melander.biz>
2022-02-13 18:35:55 +0100

    Finished Edit popup

--- src/components/Buttons/SaveButton.vue
+++ src/components/Buttons/SaveButton.vue
@@ -1,7 +1,7 @@
 <template>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <button class="delete-btn">
-        <i class="delete">Delete</i>
+    <button class="save-btn">
+        <i class="save">Save</i>
     </button>
 </template>
 <script>
@@ -10,21 +10,29 @@ export default {
 }
 </script>
 <style scoped>
-.delete-btn{
+.save-btn{
     width: 100%;
     height: 100%;
-    border-radius: 1em;
+    border-radius: 15px;
     border: none;
-    background-color: #EEB3B3;
+    background-color: #BEB3EE;
     box-shadow: 0px 3px 8px 1px #888888;
     cursor: pointer;
 }
-.delete{
+.fa-plus{
+    margin: auto auto;
+    font-size: 300%;
+    opacity: 50%;
+}
+.save{
     font-size: 25px;
     opacity: 0.8;
 }
-.delete-btn:hover {background-color:#d33d3d}
-.delete-btn:active {
+
+.save-btn:hover {
+    background-color: #9c8beb;
+}
+.save-btn:active {
   transform: translateY(4px);
 }
 </style>
